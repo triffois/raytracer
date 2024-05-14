73682004775e105185755096ddf46e48d56095d8
Romko-studik <zaletskyi.pn@ucu.edu.ua>
2024-05-14 16:45:47 +0300

    Turned off texture interpolation

--- src/main.cpp
+++ src/main.cpp
@@ -255,8 +255,9 @@ int main(int argc, char *argv[]) {
                             textures[i].height, 1, GL_RGBA, GL_UNSIGNED_BYTE,
                             textures[i].image.data());
         }
-        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+        //closest texture filtering
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_MAG_FILTER,     GL_NEAREST);
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_S,
                         GL_CLAMP_TO_EDGE);
         glTexParameteri(GL_TEXTURE_2D_ARRAY, GL_TEXTURE_WRAP_T,
